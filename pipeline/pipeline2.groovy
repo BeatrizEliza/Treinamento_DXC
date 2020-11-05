a0250a55d130b9726d61b35122613dbc84f08f38
Beatriz Eliza <beatrizelizarf@gmail.com>
2020-11-05 10:50:05 -0300

    Alterações

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -40,7 +40,7 @@ if (params.Continuar != 'Sim') {
                 currentBuild.result = 'FAILURE'
             }
 			archiveArtifacts '*.txt'
-			office365ConnectorSend message: 'Execucao Concluida.', status: 'End', webhookUrl: 'https://outlook.office.com/webhook/f1d4a645-1793-4be1-88e9-ccd82e5c37c8@93f33571-550f-43cf-b09f-cd331338d086/JenkinsCI/f4ab83d5fb7842b59f6b59fd8c0d1c0b/f1d4a645-1793-4be1-88e9-ccd82e5c37c8'
+			office365ConnectorSend message: 'Execucao Concluida.', status: 'End', webhookUrl: 'https://outlook.office.com/webhook/4ea6e32c-85f7-4079-8658-2b087fc2c014@93f33571-550f-43cf-b09f-cd331338d086/JenkinsCI/2204ac352f33484a840e82bd659f5ce7/4ea6e32c-85f7-4079-8658-2b087fc2c014'
         }
 	}
 }
